79384f90673a6452c8968e28ff0a29db25ef1bd6
leo12 <a.leo94@hotmail.com>
2022-04-25 14:45:36 -0500

    Version-1

---
+++ database/migrations/2022_03_28_155439_create_preguntas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreguntasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preguntas', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("Pregunta 1");
+            $table->string("Pregunta 2");
+            $table->string("Pregunta 3");
+            $table->string("Pregunta 4");
+            $table->string("Pregunta 5");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preguntas');
+    }
+}
